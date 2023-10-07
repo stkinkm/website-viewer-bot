609268a6155ba8396404604dfb56a5bc270c8280
stkinkm <144306409+stkinkm@users.noreply.github.com>
2023-10-07 18:02:34 +0700

    Update site-view-bot.py

--- site-view-bot.py
+++ site-view-bot.py
@@ -4,29 +4,28 @@ import sys
 from torrequest import TorRequest
 
 headers = {
-	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36"
 }
 
-print ("Website Viewer")
-#Default Tor port configuration
-proxyPort=9050
-ctrlPort=9051
-site = raw_input("Enter your Site Address : ")
-hits = input("Enter The number of Viewers : ")
+print("Website Viewer")
+# Default Tor port configuration
+proxyPort = 9050
+ctrlPort = 9051
+site = input("Enter your Site Address : ")
+hits = int(input("Enter The number of Viewers : "))
 
 
 def run():
-     response = tr.get(site, headers=headers,verify=False)
-     print ("["+str(i)+"]" + " Site View Added With IP:" +  tr.get('http://ipecho.net/plain').content)
-     tr.reset_identity()
-
+    response = tr.get(site, headers=headers, verify=False)
+    print("[" + str(i) + "] Site View Added With IP:" + tr.get('http://ipecho.net/plain').content)
+    tr.reset_identity()
 
 
 if __name__ == '__main__':
-	# if len(sys.argv) > 3:
-	#    if sys.argv[1] and sys.argv[2]:
-	# 		proxyPort=sys.argv[1]
-	# 		ctrlPort=sys.argv[2]
-	with TorRequest(proxy_port=proxyPort, ctrl_port=ctrlPort, password=None) as tr:
-		for i in range(hits):
-			run()
+    # if len(sys.argv) > 3:
+    #    if sys.argv[1] and sys.argv[2]:
+    #        proxyPort=sys.argv[1]
+    #        ctrlPort=sys.argv[2]
+    with TorRequest(proxy_port=proxyPort, ctrl_port=ctrlPort, password=None) as tr:
+        for i in range(hits):
+            run()
